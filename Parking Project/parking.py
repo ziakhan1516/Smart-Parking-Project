f3455191d4cde87643903c87dd2367fb81face53
ziakhan1516 <63509850+ziakhan1516@users.noreply.github.com>
2024-06-21 17:05:50 +0900

    parking.py

--- Parking Project/parking.py
+++ Parking Project/parking.py
@@ -4,6 +4,18 @@ from parking_project import*
 parking_lot_1, parking_lot_2, lot1_reserved, lot2_reserved, parking_size = parking_project()
 
 def parking(number):
+    """
+    Parks a car in one of the two parking lots.
+
+    This function attempts to park a car in the first available row of either parking lot 1 or parking lot 2.
+    It prioritizes parking lot 1 if both lots have available space in the same row.
+
+    Parameters:
+    number (int): The car number to be parked.
+
+    Returns:
+    str: A message indicating where the car was parked, or if both parking lots are full.
+    """
     global parking_lot_1, parking_lot_2
     
     for (park1_index, park1_row), (park2_index, park2_row) in zip(enumerate(parking_lot_1), enumerate(parking_lot_2)):
